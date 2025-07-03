3b3913fd7e35550c6339b86590d5096b1ecb3412
dfbakin <denis.bakin5@gmail.com>
2025-07-03 04:01:17 +0300

    init

---
+++ include/alarmclock.h
@@ -0,0 +1,30 @@
+#include <string>
+
+class AlarmClock {
+  private:
+    int hours;
+    int minutes;
+    bool activeStatus;
+    int volume;
+    std::string melody;
+  public:
+    // Конструктор по умолчанию
+    AlarmClock();
+
+    // Возвращают *this для цепочек вызовов
+    AlarmClock& setTime(int hours, int minutes);
+    AlarmClock& turnOn();
+    AlarmClock& turnOff();
+    AlarmClock& setVolume(int volume);
+    AlarmClock& setMelody(const std::string& melody);
+
+    // Геттеры
+    int getHours() const;
+    int getMinutes() const;
+    bool getActiveStatus() const;
+    int getVolume() const;
+    std::string getMelody() const;
+
+    // Вывод состояния в консоль
+    void printStatus() const;
+};
